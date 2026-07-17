d276ce9813da6f3d47e4993d1d57a59d47204913
Archana Kumari <ms.archanacusat2015@gmail.com>
2026-07-18 00:40:00 +0530

    Entity added

---
+++ app/src/main/java/com/example/roomdbdemo/Contact.kt
@@ -0,0 +1,11 @@
+package com.example.roomdbdemo
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact")
+data class Contact(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val name: String,
+    val mobile: String
+)
